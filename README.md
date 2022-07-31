599d756818e10585a38beb93c1de67a551a927e3
LK <lunakiira0911@gmail.com>
2022-07-30 22:38:35 -0700

    update readme and other connection.js

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# HW18-LK-Social-Network-API
+ 
+
+# Social Network Api
+
+## Description
+
+This is a Social Network API built using Mongoose and is ready to be hooked up to a front end
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Instructions](#instructions)
+- [Questions](#questions)
+
+## Installation
+
+In order to install this project you must clone this project on to your local machine.
+
+## Usage for program
+
+This project usage is for anyone that needs a back-end for their social network type site
+
+## The Instructions
+
+- If you would like to view a video on how to install and use, go to my [insert video](https://--- )
+
+- Step 1: Clone this repository
+
+* ![1st step](image1)
+
+- Step 2: Install dependencies with 'npm install'
+
+* ![2n step](image2)
+
+- Step 3: Type 'npm start' once more to start the server
+
+* ![3rd step](image3)
+
+- Step 4: Open Insomnia core/postman
+
+* ![4th step](image4)
+
+ - 
+## Questions
+
+Created by: [Luna Kiira](https://github.com/lunakiira)
+
+If you have any further questions please feel free to contact me at [lunakiira0911@gmail.com](lunakiira0911@gmail.com)
---
+++ models/thought.js
@@ -0,0 +1,60 @@
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat')
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+const ThoughtsSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createAtval => dateFormat(createAtval)
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+const Thoughts = model('Thoughts', ThoughtsSchema);
+
+module.exports = Thoughts;
